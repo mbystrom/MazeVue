f37832e3b422ab008f2758acd47c07a63d8eddc0
Noah Bystrom <noah.bystrom@calbt.com>
2018-07-05 14:53:06 -0700

    Set up maze display and initialize grid

---
+++ src/views/Maze.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <h1>A Simple Maze</h1>
+    <span v-for="(row, index) in maze" :key="index">
+      <span v-for="(col, index) in row" :key="index">
+        <span v-if="col === '#'">&nbsp;#&nbsp;</span>
+        <span v-else>&nbsp;=&nbsp;</span>
+      </span><br/>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      height: 32,
+      width: 32,
+      maze: []
+    }
+  },
+  methods: {
+    CreateArray(height,width) {
+      var arr = []
+
+      for (var i = 0; i < height; i++) {
+        arr[i] = []
+        for (var j = 0; j < width; j++) {
+          arr[i][j] = '#'
+        }
+      }
+
+      this.maze = arr
+    },
+
+    GenerateMaze () {
+      var startRow = Math.floor(Math.random() * 32)
+      var startColumn = Math.floor(Math.random() * 32)
+      console.log(startRow + ', ' + startColumn)
+      this.maze[startRow][startColumn] = '_'
+    }
+  },
+
+  created: function () {
+    this.CreateArray(this.height, this.width)
+    this.GenerateMaze()
+  }
+}
+</script>
